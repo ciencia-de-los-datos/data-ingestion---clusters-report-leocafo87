a70a9cdfcb94fafdbaff2cf212c2d95667ff62d1
leocafo87 <leonardo.castellanos.forero@gmail.com>
2023-03-18 07:36:49 -0500

    Limpieza de datos

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,20 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    df = pd.read_fwf('clusters_report.txt', skiprows=4, skipfooter=0, names=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
 
-    return df
+    df['cluster'] = df['cluster'].fillna(method="ffill")
+
+    df['principales_palabras_clave'] = df[['cluster','principales_palabras_clave']].groupby(['cluster'])['principales_palabras_clave'].transform(lambda x: ' '.join((x)))
+    df = df.dropna()
+    df = df.reset_index()
+    
+    df = df.replace(r'\s+', ' ', regex=True)
+    del df ["index"]
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace('.','')
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str[:-2]
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',','.')
+    df['porcentaje_de_palabras_clave'] = pd.to_numeric(df['porcentaje_de_palabras_clave'])
+    dff = df.astype({'principales_palabras_clave':'string'})
+
+    return dff
